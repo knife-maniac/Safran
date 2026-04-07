29f9479c6f8bb62e962b70d97edcb762b143814c
Ruben Quast <Ruben.Quast@outlook.fr>
2026-04-07 21:17:10 +0200

    Hid articles with date in the future

--- src/feed-transformer.ts
+++ src/feed-transformer.ts
@@ -2,29 +2,31 @@ import { IItem } from './feed-extractor.js';
 
 
 export function transform(items: IItem[]): IItem[] {
-    // Remove duplicate items (URL is used as a unique identifier)
-    const uniqueItems = items.filter((item, index, self) => {
-        return index === self.findIndex((t) => t.link === item.link);
-    });
+    return items
 
-    // Limit items to the last 36 hours
-    const recentItems = uniqueItems.reduce((acc, item) => {
-        if (item.pubDate) {
-            const ageInHours = (Date.now() - Date.parse(item.pubDate)) / (1000 * 60 * 60);
-            if (ageInHours <= 36) acc.push(item);
-        }
-        return acc;
-    }, [] as IItem[]);
+        // Remove duplicate items (URL is used as a unique identifier)
+        .filter((item, index, self) => {
+            return index === self.findIndex((t) => t.link === item.link);
+        })
 
-    // Limit number of items from each feed according to its own configuration
-    const limitedItems = recentItems.reduce((acc, item) => {
-        const sourceItems = acc.filter((i) => i.feedTitle === item.feedTitle);
-        const limitNumberTo = item.feedConfiguration.limitNumberTo;
-        if (!limitNumberTo || sourceItems.length < limitNumberTo) {
-            acc.push(item);
-        }
-        return acc;
-    }, [] as IItem[]);
+        // Limit items to the last 36 hours (and remove items without a pubDate or with a pubDate in the future)
+        .reduce((acc, item) => {
+            if (item.pubDate) {
+                const ageInHours = (Date.now() - Date.parse(item.pubDate)) / (1000 * 60 * 60);
+                if (ageInHours <= 36 && ageInHours >= 0) {
+                    acc.push(item);
+                }
+            }
+            return acc;
+        }, [] as IItem[])
 
-    return limitedItems;
+        // Limit number of items from each feed according to its own configuration
+        .reduce((acc, item) => {
+            const sourceItems = acc.filter((i) => i.feedTitle === item.feedTitle);
+            const limitNumberTo = item.feedConfiguration.limitNumberTo;
+            if (!limitNumberTo || sourceItems.length < limitNumberTo) {
+                acc.push(item);
+            }
+            return acc;
+        }, [] as IItem[]);
 }
