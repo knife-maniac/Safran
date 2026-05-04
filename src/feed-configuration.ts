132161f619617f7ef6545eb405e9756d69ed9ccc
Ruben Quast <Ruben.Quast@outlook.fr>
2026-05-04 21:45:05 +0200

    Updated default demo feed configuration

--- src/feed-configuration.ts
+++ src/feed-configuration.ts
@@ -5,39 +5,74 @@ export interface IFeedConfiguration {
 }
 
 export const FEEDS: IFeedConfiguration[] = [
-    {
-        name: 'Cambridge Analytica',
-        url: 'https://cambridgeanalytica.org/feed/'
-    },
-    {
-        name: 'Wikipedia (featured article)',
-        url: 'https://en.wikipedia.org/w/api.php?action=featuredfeed&feed=featured&feedformat=rss'
-    },
+    // Entertainment
     {
         name: 'XKCD',
-        url: 'https://xkcd.com/rss.xml'
+        url: 'https://xkcd.com/rss.xml',
+        limitNumberTo: 2
+    },
+
+    // General news
+    {
+        name: 'Wikipedia (featured article)',
+        url: 'https://en.wikipedia.org/w/api.php?action=featuredfeed&feed=featured&feedformat=rss',
+        limitNumberTo: 2
+    },
+    {
+        name: 'BBC',
+        url: 'https://feeds.bbci.co.uk/news/world/rss.xml',
+        limitNumberTo: 2
     },
     {
         name: 'Ars Technica (general)',
-        url: 'https://arstechnica.com/feed/'
+        url: 'https://arstechnica.com/feed/',
+        limitNumberTo: 2
     },
+
+    // Technology news
     {
         name: 'Ars Technica (technology)',
-        url: 'https://feeds.arstechnica.com/arstechnica/technology-lab'
+        url: 'https://feeds.arstechnica.com/arstechnica/technology-lab',
+        limitNumberTo: 2
     },
     {
         name: 'TechCrunch',
         url: 'https://techcrunch.com/feed/',
-        limitNumberTo: 3
+        limitNumberTo: 2
     },
     {
-        name: 'HNRSS',
+        name: 'Hacker News',
         url: 'https://hnrss.org/frontpage',
-        limitNumberTo: 3
+        limitNumberTo: 2
     },
     {
-        name: 'HNRSS (best)',
+        name: 'Hacker News (best)',
         url: 'https://hnrss.org/best',
-        limitNumberTo: 3
+        limitNumberTo: 2
     },
+    {
+        name: 'Sid\'s Blog',
+        url: 'https://www.0xsid.com/blog/rss.xml',
+        limitNumberTo: 2
+    },
+    {
+        name: 'Anil Dash',
+        url: 'https://www.anildash.com/feed.xml',
+        limitNumberTo: 2
+    },
+    {
+        name: 'Josh Blais',
+        url: 'https://joshblais.com/index.xml',
+        limitNumberTo: 2
+    },
+    {
+        name: 'Max van IJsselmuiden',
+        url: 'https://www.maxvanijsselmuiden.nl/rss.xml',
+        limitNumberTo: 2
+    },
+    {
+        name: 'Minas Karamanis',
+        url: 'https://ergosphere.blog/atom.xml',
+        limitNumberTo: 2
+    }
 ];
--- src/feed-transformer.ts
+++ src/feed-transformer.ts
@@ -9,16 +9,16 @@ export function transform(items: IItem[]): IItem[] {
             return index === self.findIndex((t) => t.link === item.link);
         })
 
-        // Limit items to the last 36 hours (and remove items without a pubDate or with a pubDate in the future)
-        .reduce((acc, item) => {
-            if (item.pubDate) {
-                const ageInHours = (Date.now() - Date.parse(item.pubDate)) / (1000 * 60 * 60);
-                if (ageInHours <= 36 && ageInHours >= 0) {
-                    acc.push(item);
-                }
-            }
-            return acc;
-        }, [] as IItem[])
+        // // Limit items to the last 36 hours (and remove items without a pubDate or with a pubDate in the future)
+        // .reduce((acc, item) => {
+        //     if (item.pubDate) {
+        //         const ageInHours = (Date.now() - Date.parse(item.pubDate)) / (1000 * 60 * 60);
+        //         if (ageInHours <= 36 && ageInHours >= 0) {
+        //             acc.push(item);
+        //         }
+        //     }
+        //     return acc;
+        // }, [] as IItem[])
 
         // Limit number of items from each feed according to its own configuration
         .reduce((acc, item) => {
