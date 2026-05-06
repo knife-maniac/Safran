f8e49f69a756e94e43b4ee03abdbd91b73d8fe82
Ruben Quast <Ruben.Quast@outlook.fr>
2026-05-06 21:23:03 +0200

    Moved "timeAgo" function to new utils.js file

--- public/index.js
+++ public/index.js
@@ -1,20 +1,3 @@
-function timeAgo(dateAsString) {
-    const date = new Date(dateAsString);
-    const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
-    if (seconds < 10) return 'just now';
-    if (seconds < 60) return `${seconds} seconds ago`;
-    const minutes = Math.floor(seconds / 60);
-    if (minutes < 60) return `${minutes} minute${minutes === 1 ? '' : 's'} ago`;
-    const hours = Math.floor(minutes / 60);
-    if (hours < 24) return `${hours} hour${hours === 1 ? '' : 's'} ago`;
-    const days = Math.floor(hours / 24);
-    if (days < 30) return `${days} day${days === 1 ? '' : 's'} ago`;
-    const months = Math.floor(days / 30);
-    if (months < 12) return `${months} month${months === 1 ? '' : 's'} ago`;
-    const years = Math.floor(months / 12);
-    return `${years} year${years === 1 ? '' : 's'} ago`;
-}
-
 function createItemElement(item) {
     const li = document.createElement('li');
     li.className = 'item';
@@ -138,7 +121,7 @@ async function load() {
     try {
         // Fetch the feed items from the backend API
         // TODO: Use URL configured in .env file
-        const res = await fetch('/projects/safran/feed.json', { cache: 'no-store' });
+        const res = await fetch('/projects/54fr4n/feed.json', { cache: 'no-store' });
         if (!res.ok) throw new Error(await res.text());
 
         // Parse the JSON response
---
+++ public/utils.js
@@ -0,0 +1,16 @@
+function timeAgo(dateAsString) {
+    const date = new Date(dateAsString);
+    const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+    if (seconds < 10) return 'just now';
+    if (seconds < 60) return `${seconds} seconds ago`;
+    const minutes = Math.floor(seconds / 60);
+    if (minutes < 60) return `${minutes} minute${minutes === 1 ? '' : 's'} ago`;
+    const hours = Math.floor(minutes / 60);
+    if (hours < 24) return `${hours} hour${hours === 1 ? '' : 's'} ago`;
+    const days = Math.floor(hours / 24);
+    if (days < 30) return `${days} day${days === 1 ? '' : 's'} ago`;
+    const months = Math.floor(days / 30);
+    if (months < 12) return `${months} month${months === 1 ? '' : 's'} ago`;
+    const years = Math.floor(months / 12);
+    return `${years} year${years === 1 ? '' : 's'} ago`;
+}
