54aab33baeec8e96f22813ec91da6667ad5dd0c6
Ruben Quast <Ruben.Quast@outlook.fr>
2026-05-18 00:20:18 +0200

    Aligned diagnosis page with new JSON interface

--- public/diagnosis/diagnosis.js
+++ public/diagnosis/diagnosis.js
@@ -1,6 +1,6 @@
-function createItemElement(feed) {
+function createFeedElement(feedData, error) {
     const li = document.createElement('li');
-    li.className = `item ${feed.error ? 'error' : 'success'}`;
+    li.className = `item ${error ? 'error' : 'success'}`;
 
     // Header div
     const header = document.createElement('div');
@@ -8,20 +8,20 @@ function createItemElement(feed) {
 
     // Feed home link
     let feedTitleContainer = header;
-    if (feed.homeLink) {
+    if (feedData.homeLink) {
         const feedPageLink = document.createElement('a');
         feedPageLink.className = 'feed-home-link';
-        feedPageLink.href = feed.homeLink;
+        feedPageLink.href = feedData.homeLink;
         header.appendChild(feedPageLink);
         feedTitleContainer = feedPageLink;
     }
 
     // Feed icon
-    if (feed.icon) {
+    if (feedData.feedIcon) {
         let icon = document.createElement('img');
         icon.className = 'feed-icon';
-        icon.src = feed.icon;
-        icon.alt = feed.name ? `${feed.name} icon` : 'Feed icon';
+        icon.src = feedData.feedIcon;
+        icon.alt = feedData.name ? `${feedData.name} icon` : 'Feed icon';
         feedTitleContainer.appendChild(icon);
         icon.onerror = () => {
             icon.onerror = null;
@@ -31,15 +31,15 @@ function createItemElement(feed) {
 
     // Feed title
     const feedTitle = document.createElement('span');
-    feedTitle.textContent = feed.name || feed.url;
+    feedTitle.textContent = feedData.name || feedData.url;
     feedTitle.className = 'feed-title';
     feedTitleContainer.appendChild(feedTitle);
 
     // Source icon
     let sourceIcon = document.createElement('a');
     sourceIcon.className = 'feed-icon feed-source-icon';
-    sourceIcon.href = feed.url;
-    sourceIcon.alt = feed.name ? `${feed.name} source icon` : 'Source icon';
+    sourceIcon.href = feedData.url;
+    sourceIcon.alt = feedData.name ? `${feedData.name} source icon` : 'Source icon';
     header.appendChild(sourceIcon);
     sourceIcon.onerror = () => {
         sourceIcon.onerror = null;
@@ -52,7 +52,10 @@ function createItemElement(feed) {
     // Description
     const description = document.createElement('span');
     description.className = 'description';
-    const status = feed.error || `fetched ${feed.numberOfItemsFetched} items (${feed.limitNumberTo} shown)`;
+    let status = error || `OK`;
+    if (feedData.limitNumberTo) {
+        status += ` (${feedData.limitNumberTo} shown)`;
+    }
     description.textContent = status;
     li.appendChild(description);
 
@@ -67,22 +70,21 @@ async function load() {
 
     try {
         // Fetch the feed items from the backend API
-        // TODO: Use URL configured in .env file
         const res = await fetch('../feed.json', { cache: 'no-store' });
         if (!res.ok) throw new Error(await res.text());
 
         // Parse the JSON response
-        const { feeds, fetchedAt } = await res.json();
+        const { feeds, fetchedAt, timeToFetchInMs } = await res.json();
         status.textContent = '';
         if (fetchedAt && fetchedEl) {
-            fetchedEl.textContent = `Updated ${timeAgo(fetchedAt)}`;
+            fetchedEl.textContent = `Updated ${timeAgo(fetchedAt)}, took ${(timeToFetchInMs / 1000).toFixed(2)} seconds`;
         } else if (fetchedEl) {
             fetchedEl.textContent = '';
         }
 
         // Create and append list feeds
-        feeds.forEach(feed => {
-            const li = createItemElement(feed);
+        feeds.forEach(({ error, feedData }) => {
+            const li = createFeedElement(feedData, error);
             list.appendChild(li);
         });
     } catch (err) {
